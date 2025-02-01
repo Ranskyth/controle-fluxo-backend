41cc7f7cbaa5fdcdef3bfac2ba32d64e1eff202d
Gabriel <ga.lima2205@gmail.com>
2025-01-31 23:30:25 -0300

    feat: update in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import express from 'express';
-import { routerProdutos } from './routes/produtosRoutes';
-import cors from 'cors'
+import express, { Request, Response, NextFunction } from "express";
+import { routerProdutos } from "./routes/produtosRoutes";
+import cors from "cors";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const app = express();
-app.use(cors())
+
+app.use((req: Request, res: Response,next:NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  app.use(cors());
+  next()
+});
+
 
 app.use(express.json());
 
-app.use("/api/",routerProdutos)
+app.use("/api/", routerProdutos);
 
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/routes/produtosRoutes.ts
+++ src/routes/produtosRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import {CreateProduto, DeleteProdutoId, GetProdutosAll, GetProdutosId, UpdateProdutosId} from "../controllers/produtoController";
+import {
+  CreateProduto,
+  DeleteProdutoId,
+  GetProdutosAll,
+  GetProdutosId,
+  UpdateProdutosId,
+} from "../controllers/produtoController";
 
-export const routerProdutos = Router()
+export const routerProdutos = Router();
 
 routerProdutos.get("/produtos", GetProdutosAll);
-routerProdutos.get("/produtos/:id", GetProdutosId)
+routerProdutos.get("/produtos/:id", GetProdutosId);
 routerProdutos.post("/produtos", CreateProduto);
-routerProdutos.put("/produtos/:id", UpdateProdutosId)
+routerProdutos.put("/produtos/:id", UpdateProdutosId);
 routerProdutos.delete("/produtos/:id", DeleteProdutoId);
